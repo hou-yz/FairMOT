eff5c75640aac29ceb4527d9f6557f4bb5438433
Yunzhong Hou <hou_yz@outlook.com>
2021-06-11 02:18:27 +1000

    training update

--- src/train.py
+++ src/train.py
@@ -2,20 +2,16 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import _init_paths
-
 import os
 
-import json
 import torch
 import torch.utils.data
 from torchvision.transforms import transforms as T
-from opts import opts
-from models.model import create_model, load_model, save_model
-from models.data_parallel import DataParallel
-from logger import Logger
-from datasets.dataset_factory import get_dataset
-from trains.train_factory import train_factory
+from lib.opts import opts, update_dataset_info_and_set_heads
+from lib.models.model import create_model, load_model, save_model
+from lib.logger import Logger
+from lib.datasets.mot_cattle import MOTDataset
+from lib.trains.mot import MotTrainer
 
 
 def main(opt):
@@ -23,20 +19,12 @@ def main(opt):
     torch.backends.cudnn.benchmark = not opt.not_cuda_benchmark and not opt.test
 
     print('Setting up data...')
-    Dataset = get_dataset(opt.dataset, opt.task)
-    f = open(opt.data_cfg)
-    data_config = json.load(f)
-    trainset_paths = data_config['train']
-    dataset_root = data_config['root']
-    f.close()
     transforms = T.Compose([T.ToTensor()])
-    dataset = Dataset(opt, dataset_root, trainset_paths, (1088, 608), augment=True, transforms=transforms)
-    opt = opts().update_dataset_info_and_set_heads(opt, dataset)
+    dataset = MOTDataset(opt, '/home/houyz/Data/cattle/train', augment=True, transforms=transforms)
+    opt = update_dataset_info_and_set_heads(opt, dataset)
     print(opt)
 
     logger = Logger(opt)
-
-    os.environ['CUDA_VISIBLE_DEVICES'] = opt.gpus_str
     opt.device = torch.device('cuda' if opt.gpus[0] >= 0 else 'cpu')
 
     print('Creating model...')
@@ -56,8 +44,7 @@ def main(opt):
     )
 
     print('Starting training...')
-    Trainer = train_factory[opt.task]
-    trainer = Trainer(opt, model, optimizer)
+    trainer = MotTrainer(opt, model, optimizer)
     trainer.set_device(opt.gpus, opt.chunk_sizes, opt.device)
 
     if opt.load_model != '':
@@ -93,6 +80,5 @@ def main(opt):
 
 
 if __name__ == '__main__':
-    torch.cuda.set_device(2)
     opt = opts().parse()
     main(opt)
