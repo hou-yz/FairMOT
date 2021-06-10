af64b67eb329fccea56efc0feb7dde5dd2fa56fc
Yunzhong Hou <hou_yz@outlook.com>
2021-06-11 02:18:45 +1000

    Update demo.py

--- src/demo.py
+++ src/demo.py
@@ -1,43 +1,36 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
-import _init_paths
-
 import logging
 import os
+import glob
 import os.path as osp
-from opts import opts
-from tracking_utils.utils import mkdir_if_missing
-from tracking_utils.log import logger
-import datasets.dataset.jde as datasets
+from lib.opts import opts
+from lib.tracking_utils.utils import mkdir_if_missing
+from lib.tracking_utils.log import logger
+import lib.datasets.jde as datasets
 from track import eval_seq
-
 
 logger.setLevel(logging.INFO)
 
 
-def demo(opt):
-    result_root = opt.output_root if opt.output_root != '' else '.'
-    mkdir_if_missing(result_root)
+def demo(opt, fname, out_fpath):
+    mkdir_if_missing(out_fpath)
 
     logger.info('Starting tracking...')
-    dataloader = datasets.LoadVideo(opt.input_video, opt.img_size)
-    result_filename = os.path.join(result_root, 'results.txt')
-    frame_rate = dataloader.frame_rate
+    dataloader = datasets.LoadVideo(fname, opt.img_size)
+    result_filename = os.path.join(out_fpath, 'results.txt')
 
-    frame_dir = None if opt.output_format == 'text' else osp.join(result_root, 'frame')
-    eval_seq(opt, dataloader, 'mot', result_filename,
-             save_dir=frame_dir, show_image=False, frame_rate=frame_rate,
-             use_cuda=opt.gpus!=[-1])
+    frame_dir = None if opt.output_format == 'text' else osp.join(out_fpath, 'frame')
+    eval_seq(opt, dataloader, 'mot', result_filename, save_dir=frame_dir, show_image=False)
 
     if opt.output_format == 'video':
-        output_video_path = osp.join(result_root, 'MOT16-03-results.mp4')
-        cmd_str = 'ffmpeg -f image2 -i {}/%05d.jpg -b 5000k -c:v mpeg4 {}'.format(osp.join(result_root, 'frame'), output_video_path)
-        os.system(cmd_str)
+        os.system(f'/usr/bin/ffmpeg -y -f image2 -i {osp.join(out_fpath, "frame")}/%05d.jpg '
+                  f'-b 5000k -c:v mpeg4 {osp.join(out_fpath, "results.mp4")}')
 
 
 if __name__ == '__main__':
-    os.environ['CUDA_VISIBLE_DEVICES'] = '0'
     opt = opts().init()
-    demo(opt)
+    root = '/home/houyz/Data/cattle/test'
+    for seq_name in sorted(os.listdir(root)):
+        if '4-05-21' in seq_name:
+            continue
+        print(seq_name)
+        demo(opt, glob.glob(f'{root}/{seq_name}/*.mp4')[0], f'../demos/{seq_name}')
